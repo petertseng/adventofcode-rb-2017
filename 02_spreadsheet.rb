aacd7dd64d1dda275cad64b26688f5cb190fce20
Peter Tseng <pht24@cornell.edu>
2017-12-02 05:08:48 +0000

    Day 02

---
+++ 02_spreadsheet.rb
@@ -0,0 +1,5 @@
+rows = ARGF.each_line.map { |l| l.split.map(&method(:Integer)) }
+
+puts rows.sum { |r| -r.minmax.reduce(:-) }
+
+puts rows.sum { |r| r.permutation(2).find { |a, b| a % b == 0 }.reduce(:/) }
