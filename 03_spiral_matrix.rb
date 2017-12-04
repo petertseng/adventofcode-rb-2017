361b0f12c75d71911bde0963e89392ea7b260647
Peter Tseng <pht24@cornell.edu>
2017-12-04 12:42:37 +0000

    Day 03: Cut corners
    Was taking 0.3 seconds, Now takes 0.08, better.

--- 03_spiral_matrix.rb
+++ 03_spiral_matrix.rb
@@ -2,7 +2,7 @@ input = Integer(!ARGV.empty? && ARGV.first.match?(/^\d+$/) ? ARGV.first : ARGF.r
 
 ORIGIN = Complex(0, 0)
 
-def spiral_matrix
+def spiral_matrix(corners_only: false)
   pos = ORIGIN
   direction = 1
   length = 1
@@ -11,19 +11,32 @@ def spiral_matrix
   Enumerator.new { |e|
     e << [1, pos]
     loop {
-      length.times { |i|
-        n += 1
-        pos += direction
+      if corners_only
+        n += length
+        pos += direction * length
         e << [n, pos]
-      }
+      else
+        length.times { |i|
+          n += 1
+          pos += direction
+          e << [n, pos]
+        }
+      end
       direction *= Complex::I
       length += 1 if direction.imag == 0
     }
   }
 end
 
-_, coord = spiral_matrix.take(input).last
-puts coord.real.abs + coord.imag.abs
+# The corners of the side of the square that contain the input,
+# and the input's distance to each:
+dists_and_corners = spiral_matrix(corners_only: true).each_cons(2).find { |(_, _), (n, _)|
+  n >= input
+}.map { |n, d| [(input - n).abs, d] }
+# The closer corner:
+dist, corner = dists_and_corners.min_by(&:first)
+# One coordinate the same as the corner, the other decreased by the distance.
+puts corner.real.abs + corner.imag.abs - dist
 
 values = {ORIGIN => 1}
 puts spiral_matrix.lazy.map { |_, pos|
