6b68c291ae1968c03f1fdf7cf7deaec40d32d479
Peter Tseng <pht24@cornell.edu>
2017-12-14 09:26:05 +0000

    Day 12: Extract BFS to lib
    Needed on multiple days.

--- 12_connected_groups.rb
+++ 12_connected_groups.rb
@@ -1,4 +1,4 @@
-require 'set'
+require_relative 'lib/search'
 
 neighbours = ARGF.each_line.to_h { |l|
   left, right = l.split('<->')
@@ -8,14 +8,7 @@ neighbours = ARGF.each_line.to_h { |l|
 puts 0.step { |i|
   break i if neighbours.empty?
 
-  queue = [neighbours.keys.first]
-  seen = Set.new
-
-  while (n = queue.pop)
-    next if seen.include?(n)
-    seen << n
-    queue.concat(neighbours[n])
-  end
+  _, seen = Search::bfs(neighbours.keys.first, neighbours, ->(_) { false })
 
   puts seen.size if seen.include?(0)
 
