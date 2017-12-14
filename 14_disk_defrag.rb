e290342f7cb4e21d11f5e185a34e16a24e3e4124
Peter Tseng <pht24@cornell.edu>
2017-12-14 10:11:54 +0000

    Day 14: Map nibbles without to_i -> to_s
    Makes no appreciable difference in runtime. Apparently all the slowness
    is performing all of the 128 * 64 * (9 + 5 + n) rotations.

--- 14_disk_defrag.rb
+++ 14_disk_defrag.rb
@@ -3,10 +3,15 @@ require_relative 'lib/union_find'
 
 GRID = 128
 
+NIBBLE = (0...16).to_h { |x|
+  bits = [8, 4, 2, 1].map { |bit| x & bit != 0 }
+  [x.to_s(16), bits.freeze]
+}.freeze
+
 grid = (0...GRID).map { |n|
   KnotHash::hash(
     "#{!ARGV.empty? && !File.exist?(ARGV.first) ? ARGV.first : ARGF.read}-#{n}".bytes + KnotHash::SUFFIX
-  ).to_i(16).to_s(2).rjust(GRID, ?0).each_char.map { |x| x == ?1 }
+  ).each_char.flat_map(&NIBBLE)
 }
 
 puts grid.sum { |row| row.count(true) }
