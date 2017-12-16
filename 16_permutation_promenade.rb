9b7a5ba8a9891482be55320d1495913ea2d25ebf
Peter Tseng <pht24@cornell.edu>
2017-12-16 07:12:05 +0000

    Day 16: decompose into absolute and relative moves
    This is really quite fantastic.
    From 1.5 seconds to about a tenth of a second.
    
    The insight comes from the fact that you can decompose the relative
    moves and absolute moves into separate permutations. If you apply them
    to themselves, you get the result of applying the original 2**n times.

--- 16_permutation_promenade.rb
+++ 16_permutation_promenade.rb
@@ -5,33 +5,53 @@ else
   16
 end
 
-def dance(orig, steps)
-  steps.each_with_object(orig.dup) { |step, progs|
+# Simply finding a single permutation matrix after one iteration is insufficient,
+# since there are both absolute and relative movements.
+# Two possible strategies:
+# 1. Find cycle length, dance 1_000_000_000 modulo cycle_length times.
+# 2. Separate the absolute and relative movements.
+#    Applying them separately gives the same result as applying them interleaved.
+#    Repeatedly applying them to themselves tells what happens if you apply them 2**n times.
+
+def moves(steps)
+  relative_moves = (0...NLETTERS).to_a
+  absolute_moves = (?a..?z).take(NLETTERS).join
+
+  steps.each { |step|
     args = step[1..-1]
     case step[0]
     when ?s
-      progs.replace(progs.chars.rotate(-Integer(args)).join)
+      relative_moves.rotate!(-Integer(args))
     when ?x
       l, r = args.scan(/\d+/).map(&:to_i)
-      progs[l], progs[r] = [progs[r], progs[l]]
+      relative_moves[l], relative_moves[r] = relative_moves.values_at(r, l)
     when ?p
       l, r = args.split(?/)
-      progs.tr!(l + r, r + l)
+      absolute_moves.tr!(l + r, r + l)
     else raise "Unknown dance step #{step}"
     end
   }
+
+  [relative_moves, absolute_moves]
 end
 
-input = ARGF.read.chomp.split(?,).map(&:freeze).freeze
+def dance(relative_moves, absolute_moves, n)
+  orig = (?a..?z).take(NLETTERS).join.freeze
 
-progs = [(?a..?z).take(NLETTERS).join.freeze]
+  n.digits(2).reduce(orig.dup) { |progs, bit|
+    if bit == 1
+      progs.tr!(orig, absolute_moves)
+      progs = Array.new(NLETTERS) { |i| progs[relative_moves[i]] }.join
+    end
 
-loop {
-  progs << dance(progs[-1], input).freeze
-  break if progs[-1] == progs[0]
-}
+    relative_moves = relative_moves.map { |r| relative_moves[r] }
+    absolute_moves = absolute_moves.tr(orig, absolute_moves)
 
-progs.pop
+    progs
+  }
+end
 
-puts progs[1]
-puts progs[1_000_000_000 % progs.length]
+moves = moves(ARGF.read.chomp.split(?,)).map(&:freeze).freeze
+
+puts dance(*moves, 1)
+puts dance(*moves, 1_000_000_000)
