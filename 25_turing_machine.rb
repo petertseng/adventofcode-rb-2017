fc82ed39d8c3bbe495bb3998de5e46e0c26e60fe
Peter Tseng <pht24@cornell.edu>
2017-12-25 09:41:30 +0000

    Day 25: Use array for FSM not hash
    Runtime from 2.3 to 2.1 seconds, in exchange for making slightly more
    assumptions. I think it's fine.
    
    This brings day 25 from being slightly the slowest to being about tied
    for the slowest with 22.
    (Assuming that 15 and 5 are going to use their C implementation).

--- 25_turing_machine.rb
+++ 25_turing_machine.rb
@@ -1,11 +1,13 @@
 def parse_input(input)
-  states = {}
+  states = []
 
   until input.empty?
     raise 'discarded non-empty line' unless input.shift.strip.empty?
 
     parsing_state = parse_state(input.shift)
-    states[parsing_state] = 2.times.map { |i|
+    # Assume input comes in order.
+    raise "parsing #{parsing_state}, wanted #{states.size}" if parsing_state != states.size
+    states << 2.times.map { |i|
       # Assume input comes in order.
       parse_rule(i, input.shift(4))
     }.freeze
@@ -29,7 +31,7 @@ def parse_rule(expect, input)
 end
 
 def parse_state(line)
-  line.chomp[-2].to_sym
+  line.chomp[-2].ord - ?A.ord
 end
 
 input = ARGF.readlines
