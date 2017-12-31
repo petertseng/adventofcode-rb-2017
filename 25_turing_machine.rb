dd867c8bf678585020b2057010e1fca42e359fca
Peter Tseng <pht24@cornell.edu>
2017-12-31 01:19:26 +0000

    Day 25: Check for +ve OOB using ||
    Speeds up by about 10%, guess the `||` is faster than `pos ==`.

--- 25_turing_machine.rb
+++ 25_turing_machine.rb
@@ -40,13 +40,13 @@ ticker = [0] * (check_after ** 0.5).ceil * 2
 pos = ticker.size / 2
 
 check_after.times {
-  ticker[pos], where_to_go, state = STATES[state][ticker[pos]]
+  ticker[pos], where_to_go, state = STATES[state][
+    ticker[pos] || 0.tap { ticker.concat([0] * ticker.size) }
+  ]
   pos += where_to_go
   if pos < 0
     pos += ticker.size
     ticker.unshift(*[0] * ticker.size)
-  elsif pos == ticker.size
-    ticker.concat([0] * ticker.size)
   end
 }
 
