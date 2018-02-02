e45a9b611eda7858c7b10418337add7d485f4c93
Peter Tseng <pht24@cornell.edu>
2018-02-02 06:32:23 +0000

    Day 22: Operate on 2x2 blocks
    Down from 2.2 seconds to 1.7 seconds, great.
    
    I tried to do this with 3x3 blocks BUT I kept them as single cells,
    which means 9 array reads. Since we stay in a 3x3 neighbourhood for
    fewer than 9 cycles, this is a loss.
    
    With 2x2 blocks, we have been measured to stay in a 2x2 neighbourhood
    for about 2 cycles, so this lets us do about half the work.

--- 22_sporifica_virus.rb
+++ 22_sporifica_virus.rb
@@ -1,53 +1,140 @@
 def grid(s, pad_size, clean, infected)
-  grid = Array.new(s.size + pad_size * 2) { Array.new(s.size + pad_size * 2, clean) }
+  raise "Size #{s.size}, but must be odd" if s.size.even?
+
+  size = (s.size + 1) / 2
+  grid = Array.new(size + pad_size * 2) { Array.new(size + pad_size * 2, clean) }
   c = {?# => infected, ?. => clean}
-  s.each_with_index { |row, y|
-    grid[pad_size + y][pad_size, row.size] = row.each_char.map(&c)
+
+  # We start pointing up, and thus must start on the bottom of a 2x2 square.
+  if (s.size / 2).even?
+    # If we would instead start on the top, shift entire grid down.
+    s = [?. * s[0].size] + s
+  else
+    s = s + [?. * s[0].size]
+  end
+
+  s.each_slice(2).with_index { |rows, y|
+    size = (rows[0].size + 1) / 2
+    grid[pad_size + y][pad_size, size] = (0...size).map { |x|
+      rows.flat_map { |row| [row[2 * x], row[2 * x + 1] || ?.] }.map(&c).reduce(0) { |acc, ch|
+        acc << 2 | ch
+      }
+    }
   }
+
   grid
 end
 
-def infects(s, n, states)
+# cache key = four squares, position inside, direction
+# 0 = up, 1 = right, 2 = down, 3 = left
+# cache key => [steps to advance, infects, infect_times, four squares, bottom bits of next cache key]
+def make_cache(states)
   # Using Hash[Symbol => Symbol] slows by about 20%,
   # so we translate to integers.
   infected = states.index(:infected)
   clean = states.index(:clean)
   flagged = states.index(:flagged)
 
+  next_state = (0...states.size).to_a.rotate(1).freeze
+
+  dir = {
+    [-1, 0] => 0,
+    [0, 1] => 1,
+    [1, 0] => 2,
+    [0, -1] => 3,
+  }.freeze
+
+  cache  = Array.new(2 ** 14)
+
+  (0...states.size).to_a.repeated_permutation(4) { |neighbourhood|
+    key_prefix = neighbourhood.reduce(0) { |acc, c| acc << 2 | c } << 4
+    [
+      [-1, 0, 1, 0],
+      [-1, 0, 1, 1],
+      [1, 0, 0, 0],
+      [1, 0, 0, 1],
+      [0, -1, 0, 1],
+      [0, -1, 1, 1],
+      [0, 1, 0, 0],
+      [0, 1, 1, 0],
+    ].each { |(dy, dx, y, x)|
+      key = key_prefix | y << 3 | x << 2 | dir[[dy, dx]]
+      raise "conflict on #{key}" if cache[key]
+
+      g = neighbourhood.each_slice(2).to_a
+      cn = 0
+      infect_times = []
+      while (0..1).cover?(x) && (0..1).cover?(y)
+        old_status = g[y][x]
+        new_status = next_state[old_status]
+        infect_times << cn if new_status == infected
+        cn += 1
+        case old_status
+        when clean
+          dy, dx = [-dx, dy]
+        when infected
+          dy, dx = [dx, -dy]
+        when flagged
+          dy *= -1
+          dx *= -1
+        end
+        g[y][x] = new_status
+        y += dy
+        x += dx
+      end
+      cache[key] = [
+        cn,
+        infect_times.size,
+        infect_times.freeze,
+        g.flatten.reduce(0) { |acc, c| acc << 2 | c },
+        (y % 2) << 3 | (x % 2) << 2 | dir[[dy, dx]],
+      ].freeze
+    }
+  }
+
+  cache.freeze
+end
+
+def infects(s, n, states)
+  sub_x = (s.size / 2) & 1
+  cache = make_cache(states)
+
   # No I'm not actually sure this padding size is provably correct
   # But instead doing a Hash[Coordinate => State] slows us by 5x-6x.
   # for 10**4, pads by 200. for 10**7, pads by 350.
   # Padding needed for the example is 188 (part 1) and 206 (part 2)
   # and for my input 26 (part 1) and 231 (part 2).
-  pad = Math.log(n, 10).ceil * 50
-  g = grid(s, pad, clean, infected)
-  y = x = g.size / 2
-  dy = -1
-  dx = 0
+  # (and because of a 2x2 grid, padding needed is halved)
+  pad = Math.log(n, 10).ceil * 25
+  g = grid(s, pad, states.index(:clean), states.index(:infected))
+
+  # If (s.size / 2) was even, grid got shifted down.
+  # If odd, it did not get shifted down; must subtract 1.
+  y = x = g.size / 2 - sub_x
   infects = 0
 
-  next_state = (0...states.size).to_a.rotate(1).freeze
+  # lower (sub_y = 1) always, by construction
+  # left (sub_x = 0) if x even, right (sub_x = 1) if x odd
+  # direction: up (0)
+  cache_key = 1 << 3 | sub_x << 2 | 0
 
-  n.times {
-    old_status = g[y][x]
-    new_status = next_state[old_status]
-    infects += 1 if new_status == infected
-    # Strangely, case/when slows by about 30%?!
-    # Maybe Integer#=== is expensive?
-    if old_status == clean
-      dy, dx = [-dx, dy]
-    elsif old_status == infected
-      dy, dx = [dx, -dy]
-    elsif old_status == flagged
-      dy *= -1
-      dx *= -1
+  dy = [-1, 0, 1, 0].freeze
+  dx = [0, 1, 0, -1].freeze
+
+  loop {
+    cache_key = g[y][x] << 4 | cache_key & 0xf
+    steps, infects_to_add, infect_times, g[y][x], cache_key = cache[cache_key]
+
+    n -= steps
+    if n < 0
+      n += steps
+      return infects + infect_times.count { |it| it < n }
     end
-    g[y][x] = new_status
-    y += dy
-    x += dx
-  }
 
-  infects
+    infects += infects_to_add
+    y += dy[cache_key & 3]
+    x += dx[cache_key & 3]
+  }
 end
 
 input = ARGF.map { |l| l.chomp.freeze }.freeze
